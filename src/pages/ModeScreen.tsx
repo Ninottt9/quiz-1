20310b324d3ccee9efc6da0aea2e70104103105d
slash122 <gleb.avrutin@gmail.com>
2023-12-09 12:09:50 +0100

    Kategorie header + ModeSreen

--- src/pages/ModeScreen.tsx
+++ src/pages/ModeScreen.tsx
@@ -7,16 +7,16 @@ export default function ModeScreen(props: {topic: string, isOpen: boolean}) {
     
     return (  <> {props.isOpen ?
         <section className="flex flex-col gap-y-5 items-center">
-           <div>
-                <p>Quiz o: {props.topic}</p>
+            <div>
+                <h1 className="mt-10 font-bold">Quiz o {props.topic}</h1>
             </div>
             <div>
                 {questionNumbers.map((number) => {
-                    return <button className="flex w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{number} pytań</button>;
+                    return <button className='flex w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-5 rounded'>{number} pytań</button>
                 })}
             </div>
             <div>
-                <button /* TODO onClick={}*/ className="flex w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Start/</button>;
+                <button /* TODO onClick={}*/ className="flex w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-20 rounded">Start</button>
             </div> 
         </section> : null }</>
     )
