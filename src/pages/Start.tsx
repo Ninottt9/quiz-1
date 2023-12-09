f3e65b854a399e65b3a5e65db665fb13d96932d2
Ninottt9 <mkazhaev746@gmail.com>
2023-12-09 10:25:31 +0100

    initial start window functionality added

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,3 +1,19 @@
 export default function Start() {
-  return <section>Start</section>;
+  const button_names = [
+    'General Knowledge',
+    'Books',
+    'Film',
+    'Music',
+    'Video Games',
+    'Mythology',
+  ];
+
+  return (
+    <section className="flex flex-col gap-y-5 items-center">
+      {button_names.map((name) => {
+        return <button className="flex w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{name}</button>;
+      })}
+      <button className="flex w-40 bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Start</button>
+    </section>
+  );
 }
